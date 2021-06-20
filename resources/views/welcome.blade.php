324f213b50943cd26829576a4bffa2a7117114a6
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2021-06-20 15:42:36 -0300

    Finalizado a estrutura básica da pagina dos inputs dos valores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,43 @@
     </style>
     <title>Conversor de Moedas - Matheus Teixeira</title>
   </head>
-  <body>
-
+  <body class="container py-5">
+    <main class="card bg-light rounded-3 p-3 text-center">
+        <h1 class="h1">Conversor de Moedas</h1>
+        <h2 class="small">Por: Matheus Teixeira</h2>
+        <div class="card-body">
+            <form class="gy-2 gx-3 mx-auto">
+                <div class="row">
+                    <div class="col-auto">
+                        <label class="visually-hidden" for="price_1">Name</label>
+                        <input class="form-control" type="number" name="price_1" id="price_1" step="any" value="1"" placeholder="Insira um valor" required>
+                    </div>
+                    <div class="col-auto">
+                      <label class="visually-hidden" for="coin_1">Preference</label>
+                      <select class="form-select" id="coin_1">
+                        <option value="BRL" selected>BRL</option>
+                        <option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                      </select>
+                    </div>
+                </div>
+                <div class="row my-3">
+                    <div class="col-auto">
+                        <label class="visually-hidden" for="price_2">Name</label>
+                        <input class="form-control" type="number" name="price_2" id="price_2" step="any" value="1"" placeholder="Insira um valor" required>
+                    </div>
+                    <div class="col-auto">
+                      <label class="visually-hidden" for="coin_2">Preference</label>
+                      <select class="form-select" id="coin_2">
+                        <option value="BRL" selected>BRL</option>
+                        <option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                      </select>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   </body>
 </html>
